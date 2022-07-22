de7ded4a666d6122baded5cb742df5e2af6debca
Vladislav Nesterenko <vladnesterenk0@mail.ru>
2022-07-22 14:03:07 +0400

    Добавьте файлы проекта.

---
+++ damage_counter/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using damage_counter;
+
+namespace damage_counter
+{
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        SwordDamage swordDamage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            swordDamage = new SwordDamage(random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7));
+            DisplayDamage();
+        }
+
+        public void RollDice()
+        {
+            swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            DisplayDamage();
+        }
+
+        void DisplayDamage()
+        {
+            damage.Text = $"Rolled {swordDamage.Roll} for {swordDamage.Damage} HP";
+        }
+
+        private void Flaming_checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Flaming = true;
+            DisplayDamage();
+        }
+
+        private void Flaming_unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Flaming = false;
+            DisplayDamage();
+        }
+
+        private void Magic_unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Magic = true;
+            DisplayDamage();
+        }
+
+        private void Magic_checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Magic = false;
+            DisplayDamage();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
+        }
+    }
+}
---
+++ damage_counter/SwordDamage.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace damage_counter
+{
+    class SwordDamage
+    {
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+
+        /// <summary>
+        /// Содержит в себе посчитанный урон
+        /// </summary>
+        public int Damage { get; private set; }
+
+        /// <summary>
+        /// Получает и устанавливает полученный ролл
+        /// </summary>
+        private int roll;
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+
+        /// <summary>
+        /// True, если меч огненный; false в противном случае
+        /// </summary>
+        private bool flaming;
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        /// <summary>
+        /// True, если меч магический; false в противном случае
+        /// </summary>
+        private bool magic;
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+
+        /// <summary>
+        /// Вычисляет повреждения в зависимости от текущих значений свойств
+        /// </summary>
+        public void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (Magic) magicMultiplier = 1.75M;
+
+            Damage = BASE_DAMAGE;
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (Flaming) Damage += FLAME_DAMAGE;
+        }
+
+        /// <summary>
+        /// Конструктор вычисляет повреждения для значений Magic и Flaming по умолчанию и начального броска 3d6
+        /// </summary>
+        /// <param name="startingRoll"></param>
+        public SwordDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+    }
+}
